d133f16b9bb8119161cc5bded9affec68b36151b
avisots <avisots@avisots.com>
2026-01-20 06:06:52 -0800

    HW07 08 09 source files

---
+++ HW08/apply.sql
@@ -0,0 +1,63 @@
+﻿USE WideWorldImporters; 
+
+--в подзапросе можно выбрать только 0 или 1 запись
+SELECT C.CustomerName, (SELECT TOP 1 OrderId
+                FROM Sales.Orders O
+                WHERE O.CustomerID = C.CustomerID
+					AND OrderDate < '2014-01-01'
+                ORDER BY O.OrderDate DESC, O.OrderID DESC)
+FROM Sales.Customers C
+ORDER BY C.CustomerName;
+
+-- сравнить кол-во строк!
+-- последние 2 заказа, cross apply похож на inner join
+SELECT C.CustomerName, O.*
+FROM Sales.Customers C
+CROSS APPLY (SELECT TOP 2 *
+                FROM Sales.Orders O
+                WHERE O.CustomerID = C.CustomerID
+					AND OrderDate < '2014-01-01'
+                ORDER BY O.OrderDate DESC, O.OrderID DESC) AS O
+ORDER BY C.CustomerName;
+
+
+--последние 2 заказа, outer apply похож на  left join
+SELECT C.CustomerName, O.*
+FROM Sales.Customers C
+OUTER APPLY (SELECT TOP 2 *
+                FROM Sales.Orders O
+                WHERE O.CustomerID = C.CustomerID
+					AND OrderDate < '2014-01-01'
+                ORDER BY O.OrderDate DESC, O.OrderID DESC) AS O
+ORDER BY C.CustomerName;
+
+
+--function call
+SELECT C.CustomerName, O.*
+FROM Sales.Customers C
+OUTER APPLY [Sales].[orders_customer](C.CustomerID) AS O
+ORDER BY C.CustomerName;
+
+
+-- читабельность
+SELECT DATEADD(hh,DATEDIFF(hh,0,GETDATE()),0), 
+		GETDATE(), 
+		DATEADD(mm,DATEDIFF(mm,0,GETDATE()),0)
+
+-- через group by и apply
+SELECT CAST(DATEADD(mm,DATEDIFF(mm,0,P.OrderDate),0) AS DATE) AS PurchaseOrderMonth,
+	COUNT(*) AS PurchaseCount
+FROM Purchasing.PurchaseOrders AS P
+GROUP BY CAST(DATEADD(mm,DATEDIFF(mm,0,P.OrderDate),0) AS DATE)
+ORDER BY CAST(DATEADD(mm,DATEDIFF(mm,0,P.OrderDate),0) AS DATE);
+
+--cross apply
+SELECT CA.PurchaseOrderMonth,
+	COUNT(*) AS PurchaseCount
+FROM Purchasing.PurchaseOrders AS P
+CROSS APPLY (
+	SELECT 
+		CAST(DATEADD(mm,DATEDIFF(mm,0,P.OrderDate),0) AS DATE) AS PurchaseOrderMonth
+	) AS CA
+GROUP BY CA.PurchaseOrderMonth
+ORDER BY CA.PurchaseOrderMonth;
---
+++ HW08/pivot_unpivot.sql
@@ -0,0 +1,159 @@
+﻿use WideWorldImporters;
+
+-- 1 строка с итогами
+SELECT * 
+FROM ( 
+	select YEAR(ord.OrderDate) as SalesYear
+		, L.UnitPrice * L.Quantity as TotalSales
+	from Sales.Orders as ord
+	join Sales.OrderLines L on ord.OrderID = L.OrderID
+	join Sales.Customers as c on c.CustomerID = ord.CustomerID 
+	) as t
+-- перед pivot должны остаться только используемые колонки 
+-- по колонкам, не задействованным в pivot, выполняется неявная группировка	
+-- до pivot 2 колонки => результат - 1 строка
+PIVOT (
+	sum(TotalSales) -- агрегирующая ф-ция
+	for SalesYear in ([2013],[2014],[2015],[2016]) -- колонки "для разворота"
+	) as pvt
+
+-- НЕЯВНАЯ (не пишем group by) группировка по клиентам
+SELECT * 
+FROM (
+	SELECT c.CustomerName -- доп колонка
+		, YEAR(ord.OrderDate) AS SalesYear
+		, L.UnitPrice * L.Quantity AS TotalSales
+	FROM Sales.Orders AS ord
+	JOIN Sales.OrderLines L ON ord.OrderID = L.OrderID
+	JOIN Sales.Customers as c on c.CustomerID = ord.CustomerID 
+	) as t
+PIVOT (
+	sum(TotalSales) 
+	for SalesYear IN ([2013],[2014],[2015],[2016]) 
+	) as pvt
+order by CustomerName
+
+
+SELECT SalesYear, 
+       ISNULL([Q1], 0) AS Q1, 
+       ISNULL([Q2], 0) AS Q2, 
+       ISNULL([Q3], 0) AS Q3, 
+       ISNULL([Q4], 0) AS Q4, 
+       (ISNULL([Q1], 0) + ISNULL([Q2], 0) + ISNULL([Q3], 0) + ISNULL([Q4], 0)) SalesYTD
+FROM
+(
+    SELECT YEAR(OH.OrderDate) AS SalesYear, 
+           CAST('Q'+CAST(DATEPART(QUARTER, OH.OrderDate) AS VARCHAR(1)) AS VARCHAR(2)) AS Quarters, 
+           SUM(L.UnitPrice*L.Quantity) AS TotalSales
+    FROM Sales.Orders OH
+    JOIN Sales.OrderLines L ON OH.OrderId = L.OrderId
+	GROUP BY YEAR(OH.OrderDate), 
+           CAST('Q'+CAST(DATEPART(QUARTER, OH.OrderDate) AS VARCHAR(1)) AS VARCHAR(2))
+ ) t
+ PIVOT(
+	SUM(TotalSales) 
+	FOR Quarters IN ([Q1], [Q2], [Q3], [Q4])
+	) AS pvt
+ORDER BY SalesYear
+
+--- error
+SELECT * 
+FROM (
+	SELECT YEAR(ord.OrderDate) AS SalesYear
+		,L.UnitPrice * L.Quantity AS TotalSales
+	FROM Sales.Orders AS ord
+	JOIN Sales.OrderLines L ON ord.OrderID = L.OrderID
+) t 
+PIVOT (
+	SUM(TotalSales), AVG(TotalSales) -- <-- 2 агрегирующие функции!!
+	FOR SalesYear IN ([2013],[2014],[2015],[2016])
+	) as PVT
+
+-- перепишем
+; with cte as (
+	SELECT YEAR(ord.OrderDate) AS SalesYear
+		,L.UnitPrice * L.Quantity AS TotalSales
+	FROM Sales.Orders AS ord
+	JOIN Sales.OrderLines L ON ord.OrderID = L.OrderID
+)
+, cte1 as (
+	select * from cte
+	pivot (
+		sum(TotalSales)
+		for SalesYear IN ([2013],[2014],[2015],[2016])
+	) pvt
+)
+, cte2 as (
+	select * from cte 
+	pivot (
+		avg(TotalSales)
+		for SalesYear IN ([2013],[2014],[2015],[2016])
+	) pvt
+)
+select t1.*
+	, t2.[2013] as [avg 2013], t2.[2014] as [avg 2014], t2.[2015] as [avg 2015], t2.[2016] as [avg 2016]
+from cte1 as t1, cte2 as t2
+
+-- вариант 2
+; with cte as (
+	SELECT YEAR(ord.OrderDate) AS SalesYear
+		,L.UnitPrice * L.Quantity AS TotalSales
+	FROM Sales.Orders AS ord
+	JOIN Sales.OrderLines L ON ord.OrderID = L.OrderID
+)
+select sum([2013]) as [2013], sum([2014]), sum([2015]), sum([2016])
+	, avg([2013]) as [avg 2013], avg([2014]), avg([2015]), avg([2016])
+from (
+	select [2013] = case SalesYear when 2013 then TotalSales end
+		, [2014] = case SalesYear when 2014 then TotalSales end
+		, [2015] = case SalesYear when 2015 then TotalSales end
+		, [2016] = case SalesYear when 2016 then TotalSales end
+	from cte 
+) t 
+
+--- unpivot
+-- pivot сохраним  в #t 
+drop table if exists #t 
+
+SELECT * 
+into #t 
+FROM ( 
+	select YEAR(ord.OrderDate) as SalesYear
+		, L.UnitPrice * L.Quantity as TotalSales
+	from Sales.Orders as ord
+	join Sales.OrderLines L on ord.OrderID = L.OrderID
+	join Sales.Customers as c on c.CustomerID = ord.CustomerID 
+	) as t
+PIVOT (
+	sum(TotalSales) -- агрегирующая ф-ция
+	for SalesYear in ([2013],[2014],[2015],[2016]) -- колонки "для разворота"
+	) as pvt
+
+select * from #t 
+SELECT *
+FROM (-- то, что получили до этого в pivot (1 строка, 4 колонки)
+	select [2013],[2014],[2015],[2016] 
+	from #t
+	) t 
+UNPIVOT( -- 2 колонки, 4 строки (=кол-ву значений в IN ())
+		-- TotalSales - произвольное название для колонки с данными из таблицы
+		TotalSales
+		-- SalesYear - произвольное название для колонки с описанием
+		for SalesYear IN ([2013],[2014],[2015],[2016])
+) AS upvt
+
+--- 4 столбца - в строки (удобнее искать по одной колонке)
+SELECT PersonID, FullName, PreferredName, LogonName
+FROM Application.People
+
+-- null - исключен: PersonId <= 2 - по 2 строки на id, далее - по 3
+SELECT *
+FROM (
+	SELECT PersonID, FullName, PreferredName, iif(PersonId <= 2, null, LogonName) as LogonName
+	FROM Application.People
+	) AS People
+UNPIVOT ( -- PersonID - не вошло в unpivot => перешло в итоговую таблицу
+	PersonName -- колонка с данными из таблицы
+	FOR Name IN (FullName, PreferredName, LogonName) -- колонка с описанием 
+) AS unpt
+
