e56009e5f91bcbd4edce1ad4e78797f6f842708c
avisots <avisots@avisots.com>
2026-05-25 08:49:46 -0700

    HW13

---
+++ HW13/CLRFunctions/CLRFunctions/RegexFunctions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+
+namespace RegexFunctions
+{
+    public class RegexFunctions
+    {
+        [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+        public static SqlBoolean RegexMatch(SqlString input, SqlString pattern)
+        {
+            if (input.IsNull || pattern.IsNull)
+                return SqlBoolean.False;
+
+            return Regex.IsMatch(input.Value, pattern.Value);
+        }
+    }
+}
---
+++ HW13/HW13.sql
@@ -0,0 +1,27 @@
+﻿EXEC sp_configure 'clr enabled', 1;
+RECONFIGURE;
+
+EXEC sp_configure 'clr strict security', 0;
+RECONFIGURE;
+
+
+CREATE ASSEMBLY RegexMatch
+FROM 'C:\SQLCLR\CLRFunctions.dll'
+WITH PERMISSION_SET = SAFE;
+
+CREATE FUNCTION dbo.RegexMatch
+(
+    @input NVARCHAR(MAX),
+    @pattern NVARCHAR(1000)
+)
+RETURNS BIT
+AS EXTERNAL NAME
+RegexMatch.[RegexFunctions.RegexFunctions].RegexMatch;
+
+--Валидация адреса почты
+
+    --1 Корректный адрес:
+    print dbo.RegexMatch('test@mail.ru','^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+
+    --2 Не корректный адрес:
+    print dbo.RegexMatch('test.ru','^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
